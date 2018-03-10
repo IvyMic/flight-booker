ff3ec88d63b93eedad3c40ccaae1f495e2fa8cd6
Ivy Mic <ibrahim_e@hotmail.co.uk>
2018-03-10 14:29:53 +0000

    created basic airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Airport.delete_all
+
+Airport.create!(name: "London Heathrow", code: "LHR")
+Airport.create!(name: "London Standsted", code: "STN")
+Airport.create!(name: "London Gatwick", code: "LGW")
+Airport.create!(name: "Tangier Ibn Battouta", code: "TNG")
+Airport.create!(name: "Suvarnabhumi", code: "BKK")
+Airport.create!(name: "Los Angeles International", code: "LAX")
+Airport.create!(name: "Cape Town International Airpor", code: "CPT")
+Airport.create!(name: "Dublin", code: "DUB")
